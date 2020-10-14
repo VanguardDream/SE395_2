ce720a4bab6de74669aeba13582d1a028449d696
Bongsub Song <doorebong@gmail.com>
2020-10-14 16:45:45 +0900

    mac..

--- HW1.py
+++ HW1.py
@@ -12,14 +12,22 @@ import Loader
 import network as nw
 import dnn_app_utils_v2 as util
 
+macos = True
 
 codePath = os.path.dirname( os.path.abspath("HW1.py"))
 
-fp_trainImage = open(codePath+'\\data\\train\\train-images.idx3-ubyte','rb')
-fp_trainLabel = open(codePath+'\\data\\train\\train-labels.idx1-ubyte','rb')
+if not macos:
+    fp_trainImage = open(codePath+'\\data\\train\\train-images.idx3-ubyte','rb')
+    fp_trainLabel = open(codePath+'\\data\\train\\train-labels.idx1-ubyte','rb')
 
-fp_testImage = open(codePath+'\\data\\test\\test-images.idx3-ubyte','rb')
-fp_testLabel = open(codePath+'\\data\\test\\test-labels.idx1-ubyte','rb')
+    fp_testImage = open(codePath+'\\data\\test\\test-images.idx3-ubyte','rb')
+    fp_testLabel = open(codePath+'\\data\\test\\test-labels.idx1-ubyte','rb')
+else :
+    fp_trainImage = open(codePath+'/data/train/train-images.idx3-ubyte','rb')
+    fp_trainLabel = open(codePath+'/data/train/train-labels.idx1-ubyte','rb')
+
+    fp_testImage = open(codePath+'/data/test/test-images.idx3-ubyte','rb')
+    fp_testLabel = open(codePath+'/data/test/test-labels.idx1-ubyte','rb')
 
 # Jump MNIST file header
 tmp = fp_trainImage.read(16)
@@ -30,6 +38,12 @@ tmp = fp_testLabel.read(8)
 
 train_x, train_y, classes = Loader.load(50, fp_trainImage, fp_trainLabel)
 test_x, test_y, classes = Loader.load(50, fp_testImage, fp_testLabel)
+
+a = np.reshape([0, 0.1, 0.9],(3,1))
+b = np.reshape([0, 0, 1],(3,1))
+
+print(util.compute_cost(a,b))
+sys.exit()
 
 n_x = 28*28     # num_px * num_px * 3
 n_h = 2
--- dnn_app_utils_v2.py
+++ dnn_app_utils_v2.py
@@ -285,7 +285,8 @@ def compute_cost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    cost = (-1/m) * np.sum(Y * np.log(AL) + (1 - Y) * (np.log(1 - AL)))
+    # cost = (-1/m) * np.sum(Y * np.log(AL) + (1 - Y) * (np.log(1 - AL))) For sigmoid activation
+    cost = (-1/m) * np.sum(Y * np.log(AL))
     # cost = (1./m) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
     
     cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
